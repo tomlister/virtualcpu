3014aae61576240a3598d46e92e3d3e3d2e96e58
Tom Lister <tomlister@users.noreply.github.com>
2018-04-27 09:20:13 +1000

    Added registers.

--- cpu.js
+++ cpu.js
@@ -6,11 +6,23 @@ function load(input) {
 }
 function execute(callback) {
 	//program memory range 0x0 (0) - 0xB7 (183)
+	var registers = {
+		0xe0: 0x0, //ax
+		0xe1: 0x0, //al
+		0xe2: 0x0, //ah
+		0xe3: 0x0 //es
+	}
 	for (var i = 0; i < 183; i++) {
 		var kill = false;
 		if (global.ram[i] == 0x01) { //mov
-			global.ram[global.ram[i+1]] = global.ram[i+2];
-			i += 2;
+			//global.ram[global.ram[i+1]] = global.ram[i+2];
+			if (global.ram[i+1] == 0xd0 && global.ram[i+3] == 0xd0) {
+				global.ram[registers[global.ram[i+2]]] = global.ram[i+4];
+				i += 4;
+			} else {
+				registers[global.ram[i+1]] = global.ram[i+2];
+				i += 2;
+			}
 		} else if (global.ram.length == i) { //end
 			console.log(global.ram[i]);
 			kill = true;
--- main.js
+++ main.js
@@ -5,7 +5,8 @@ var vga = require("./vga.js")
 function main() {
 	ram.init();
 	vga.start();
-	cpu.load("01b84801b96501ba79");
+	//cpu.load("01b84801b96501ba79");
+	cpu.load("01e0b801d0e0d048");
 	cpu.execute();
 }
 main();
